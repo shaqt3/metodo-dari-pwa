60a9580565ad95fe2bedc43148d9b9ba05ad2e9f
Angélica Dueñas <103256746+shaqt3@users.noreply.github.com>
2026-07-14 15:48:42 +0200

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+import withPWAInit from 'next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+export default withPWA({});
